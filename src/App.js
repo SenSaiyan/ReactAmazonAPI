8adbb1238ae27f420cc57611ba9f41442203d0c0
Daniel Jiang <djiang@ANSI.org>
2020-01-22 10:50:54 -0500

    test

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function JSONCall(standardName) {
+  var amazon = require('amazon-affiliate-api');
+  var client = amazon.createClient({
+    awsId: "AKIAI3WW6Y5EI2PSNIKA", // access key id
+    awsSecret: "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", //secret access key
+    awsTag: "amerinatiosta-20" //associate tag
+  });
+  var JSONUrl = "https://notstaging.com/SKUtoASINKeyed.json"; //need to host on webstore server
+  var request = new XMLHttpRequest();
+  request.open('GET', JSONUrl, true);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    var uncutASIN = JSON.stringify(data[standardName][0]).split(":")[1];
+    var cutASIN = (uncutASIN.substring(1, uncutASIN.length-2));
+    client.itemLookup({
+      idType: 'ASIN',
+      itemId: cutASIN,
+      responseGroup: 'Images,ItemAttributes,OfferFull'
+    }, function(err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(results);
+      }
+    });
+    console.log("client: "+client);
+    return client;
+  }
+}
+
 function App() {
+  var ASIN = window.standardName;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    JSONCall(standardName);
   );
 }
 
