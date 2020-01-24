8bb02279b487f4b28866a18e743fe1e1260dcd66
Daniel Jiang <djiang@ANSI.org>
2020-01-24 14:35:49 -0500

    need to call client.itemlookup results

--- src/App.js
+++ src/App.js
@@ -8,38 +8,36 @@ function JSONCall(standardName) {
     awsSecret: "o365sZ9qOTWq8Q1OQfgRs7NMkds2lvyc3k8EBX2o", //secret access key
     awsTag: "amerinatiosta-20" //associate tag
   });
-  var JSONUrl = "../public/SKUtoASINKeyed.json"; //need to host on webstore server
-  var request = new XMLHttpRequest();
-  request.open('GET', JSONUrl, true);
-  request.responseType = 'json';
-  request.send();
-  request.onload = function() {
-    var data = request.response;
-    var uncutASIN = JSON.stringify(data[standardName][0]).split(":")[1];
-    var cutASIN = (uncutASIN.substring(1, uncutASIN.length-2));
-    client.itemLookup({
-      idType: 'ASIN',
-      itemId: cutASIN,
-      responseGroup: 'Images,ItemAttributes,OfferFull'
-    }, function(err, results) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(results);
-      }
-    });
-    console.log("client: "+client);
-    // return client;
-  }
+  
+  var data = require("./SKUtoASINKeyed.json");
+  // console.log(data);
+  var uncutASIN = JSON.stringify(data[standardName][0]).split(":")[1];
+  var ASIN = (uncutASIN.substring(1, uncutASIN.length-2));
+  console.log(ASIN);
+
+  client.itemLookup({
+    idType: 'ASIN',
+    itemId: ASIN,
+    responseGroup: 'Images,ItemAttributes,OfferFull'
+  }, function(err, results) {
+    // console.log("client item lookup")
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("success "+results);
+    }
+  });
 }
 
 function App() {
   var standardName = window.standardName;
-  JSONCall(standardName);
+  
   return (
-    <div>
-      <p>testestestest</p>
-    </div>
+    <body>
+      <div>
+        {JSONCall('IEC/PAS 61076-3-109 Ed. 1.0 en:2001')}
+      </div>
+    </body>
   );
 }
 
