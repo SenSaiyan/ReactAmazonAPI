543c765bde76309b31a9a18855ec9051d91622b8
Daniel Jiang <djiang@ANSI.org>
2020-01-28 14:57:40 -0500

    jan 28

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import React from 'react';
 import './App.css';
 
 function JSONCall(standardName) {
+  // var express = require('express');
+  // var cors = require('cors')
+  // var app = express()
+   
+  // app.use(cors())
+   
+  // app.get('/products/:id', function (req, res, next) {
+  //   res.json({msg: 'This is CORS-enabled for all origins!'})
+  // })
+   
+  // app.listen(80, function () {
+  //   console.log('CORS-enabled web server listening on port 80')
+  // })
+
+  // const express = require('express');
+  // var cors = require('cors-express');
+  // var app = express(),
+  //   options = {};
+ 
+  // app.use(cors(options));
+
+  // end cors package
+
   var amazon = require('amazon-affiliate-api');
   var client = amazon.createClient({
     awsId: "AKIAI3WW6Y5EI2PSNIKA", // access key id
@@ -18,7 +41,7 @@ function JSONCall(standardName) {
     itemId: ASIN,
     responseGroup: 'Images,ItemAttributes,OfferFull'
   }, function(err, results) {
-    console.log("client item lookup "+ASIN)
+    console.log("client item lookup "+ASIN);
     if (err) {
       // let error = new Error('test')
       console.log("error: "+err);
@@ -26,6 +49,14 @@ function JSONCall(standardName) {
       console.log("success!: "+results);
     }
   });
+//   client.itemLookup({
+//     idType: 'ASIN',
+//     itemId: ASIN
+//   }).then(function(results) {
+//     console.log(results);
+//   }).catch(function(err) {
+//     console.log(err);
+//   });
 }
 
 function App() {
